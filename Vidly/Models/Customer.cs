0409fdfb3d456f386a4afa6b788b85484556aad1
TakudzwaN <TakudzwaN@users.noreply.github.com>
2018-10-22 20:48:30 +0200

    Add MembershipTypes.cs & Seed MembershipTypes

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -8,7 +8,9 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }    
-
+        public string Name { get; set; }
+        public bool IsSubscribed { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }  
     }
 }
