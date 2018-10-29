44c4d597deb11a301c11c53b51c7f59f80ac7844
TakudzwaN <TakudzwaN@users.noreply.github.com>
2018-10-29 13:32:36 +0200

    Customer Edit & update form added

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -22,24 +22,36 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
+
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribed = customer.IsSubscribed;
+            }
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
         }
+
         // GET: Customers
         public ActionResult Index()
         {
@@ -60,6 +72,22 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers
+                .SingleOrDefault(c => c.Id == id);      //Edit customer by id
+
+            if (customer == null)                       //If customer doesnt exist
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);              //Return New Customer Form Using CustomerFormViewModel 
         }
     }
 }
